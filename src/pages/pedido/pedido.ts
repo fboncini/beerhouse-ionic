c47d0deb7542fcbbfa34edb88b65aa8a65db9787
fboncini <fboncini@hexacta.com>
2018-08-24 16:01:58 -0300

    pantalla pedido

--- src/pages/pedido/pedido.ts
+++ src/pages/pedido/pedido.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Menu } from '../../Menu';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the PedidoPage page.
@@ -15,11 +17,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PedidoPage {
 
+  menu: Menu[];
+  pedido: Menu[] = [];
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.menu = this.navParams.get('data');
+    this.filterCategory();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PedidoPage');
+  }
+
+  isThereAnyInCategory (category : Menu) :boolean{
+    let result = false;
+    category.products.forEach(product => {
+      product.cantity != 0 ? result = true : true; 
+    });
+    return result; 
+  }
+
+  filterCategory () {
+    this.menu.forEach(category => {
+      this.isThereAnyInCategory(category) ? this.pedido.push(category) : true;
+    });
+  }
+
+  add(id : number) {
+    this.menu.forEach(
+      element => element.products.forEach(product => {
+        if(product.id == id)
+        {
+          product.cantity ++;
+        }
+      }))
+  }
+
+  remove(id : number)
+  {
+    this.menu.forEach(
+      element => element.products.forEach(product => {
+        if(product.id == id)
+        {
+          if(product.cantity > 0)
+          {
+            product.cantity --;
+          }
+        }
+      }))
+  }
+
+  goTo() 
+  {
+    this.navCtrl.push(HomePage)
   }
 
 }
